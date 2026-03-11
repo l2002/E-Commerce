7d33332c17f61af7aadf2612a1c5c0206bef095a
l2002 <luannguyen0515@gmail.com>
2026-03-11 13:41:20 +0700

    Tạo Folder Structure SCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css';
-import Hello from '@components/Hello/Hello';
+import '@styles/main.scss';
+import MainLayout from '@components/Layout/Layout';
+import MyHeader from '@components/Header/Header';
+import MyFooter from '@components/Footer/Footer';
 
 function App() {
     return (
-        <div className='App'>
-            <Hello />
-            Ecommerse
-        </div>
+        <>
+            <MainLayout>
+                <MyHeader />
+                Content
+                <MyFooter />
+            </MainLayout>
+        </>
     );
 }
 
